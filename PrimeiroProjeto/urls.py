82550e11938862fea99b77b35444827b5bd399d5
amandasacchi <amandasacchi@hotmail.com>
2019-04-26 11:52:37 -0300

    Exemplo de Projeto - Aula 03 de Python

--- PrimeiroProjeto/urls.py
+++ PrimeiroProjeto/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from meuSite import views #Tudo que tiver dentro de views ele vai mostrar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index), #dentro da views chamar função
+    path('sobre/', views.sobre), #sobre/ é o endereço no site !!!SEMPRE POR VIRGULA!!!
 ]
--- meuSite/views.py
+++ meuSite/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request): #Request = Requisição de alguma coisa
+    return render(request, 'index.html')
+
+    
+def sobre(request):
+    return render(request, 'sobre.html')
