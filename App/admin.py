80930841dfa542ff40b9950f78eec2eb6e84dac9
a <amandasacchi@hotmail.com>
2019-05-07 08:39:02 -0300

    Criar pastas das aulas

--- App/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from App.models import Crush
-# Register your models here.
-admin.site.register(Crush)
--- App/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from App.models import Crush
-# Create your views here.
-def index(request):
-    crush = Crush.objects.all() #VAI RETORNAR TODOS OS NOSSO CRUSHES
-    contexto = {
-        'crush' : crush
-        }
-    return render(request, 'index.html', contexto)
